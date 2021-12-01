9e2c0b85b1aacb0a4ef656866259879a20cd5de4
Hearty, Oh <heartyoh@hatiolab.com>
2021-12-02 00:40:02 +0900

    fix: things-scene upgrade

--- packages/i18n/src/editors/modeller-ui.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement } from 'lit'
-
-declare module '@things-factory/modeller-ui' {
-  class ThingsEditorProperty extends LitElement {}
-}
--- packages/i18n/src/editors/property-editor-i18n.ts
+++ packages/i18n/src/editors/property-editor-i18n.ts
@@ -1,11 +1,11 @@
 import './editor-i18n'
 
-import { ThingsEditorProperty } from '@things-factory/modeller-ui'
+import { OxPropertyEditor } from '@operato/property-editor'
 import { customElement } from 'lit/decorators.js'
 import { html } from 'lit'
 
 @customElement('property-editor-i18n')
-export class PropertyEditorLegendStatus extends ThingsEditorProperty {
+export class PropertyEditorLegendStatus extends OxPropertyEditor {
   editorTemplate(props: any) {
     return html` <editor-i18n .value=${props.value} fullwidth></editor-i18n> `
   }
